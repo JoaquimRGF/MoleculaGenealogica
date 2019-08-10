0616d3d4bec188bb893a3fb604e5c47916a85f06
JOAQUIMRGF <joaquim.rgf@gmail.com>
2019-08-10 22:16:21 +0100

    Update function - UnionSerializers

--- rest_api/models.py
+++ rest_api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, null=True, blank=True)
     create_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- rest_api/serializers.py
+++ rest_api/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework import exceptions
+
 from .models import Person, Union, Family
 
 
@@ -28,17 +30,44 @@ class UnionSerializer(serializers.ModelSerializer):
             'person_two',
         ]
 
+    def validate(self, data):
+        person_one = data.get("person_one", "")
+
+        if person_one['name'] == "":
+            msg = "person_one name cannot be null"
+            raise exceptions.ValidationError(msg)
+
+        return data
+
     def create(self, validated_data):
 
         person_one = validated_data['person_one']
         person_one_obj = Person.objects.get_or_create(**person_one)
 
         person_two = validated_data['person_two']
-        person_two_obj = Person.objects.get_or_create(**person_two)
-
-        union = Union.objects.get_or_create(person_one=person_one_obj[0], person_two=person_two_obj[0])
+        if person_two["name"] != "":
+            person_two_obj = Person.objects.get_or_create(**person_two)
+            union = Union.objects.get_or_create(person_one=person_one_obj[0], person_two=person_two_obj[0])
+        else:
+            union = Union.objects.get_or_create(person_one=person_one_obj[0], person_two=None)
 
         return union[0]
+
+    def update(self, instance, validated_data):
+
+        person_one = validated_data.pop('person_one')
+
+        person_one_obj = Person.objects.get_or_create(**person_one)
+        instance.person_one = person_one_obj[0]
+
+        person_two = validated_data.pop('person_two')
+
+        if person_two['name'] != "":
+            person_two_obj = Person.objects.get_or_create(**person_two)
+            instance.person_one = person_two_obj[0]
+
+        instance.save()
+        return instance
 
 
 class FamilySerializer(serializers.ModelSerializer):
@@ -60,9 +89,12 @@ class FamilySerializer(serializers.ModelSerializer):
         children = validated_data['children']
 
         person_one_obj = Person.objects.get_or_create(**union['person_one'])
-        person_two_obj = Person.objects.get_or_create(**union['person_two'])
 
-        union = Union.objects.get_or_create(person_one=person_one_obj[0], person_two=person_two_obj[0])
+        if union['person_two']['name'] != "":
+            person_two_obj = Person.objects.get_or_create(**union['person_two'])
+            union = Union.objects.get_or_create(person_one=person_one_obj[0], person_two=person_two_obj[0])
+        else:
+            union = Union.objects.get_or_create(person_one=person_one_obj[0], person_two=None)
 
         family = Family(union=union[0])
         family.save()
@@ -96,26 +128,26 @@ class LinksSerializer(serializers.ModelSerializer):
                 "strength": 1
             })
 
-            for chil in obj.children.all():
+            for child in obj.children.all():
                 data.append({
                     "source": obj.union.person_one.id,
-                    "target": chil.id,
+                    "target": child.id,
                     "type": "children",
-                    "strength": 0.5
+                    "strength": 0.2
                 })
                 data.append({
                     "source": obj.union.person_two.id,
-                    "target": chil.id,
+                    "target": child.id,
                     "type": "children",
-                    "strength": 0.5
+                    "strength": 0.2
                 })
 
         else:
-            for chil in obj.children.all():
+            for child in obj.children.all():
                 data.append({
                     "source": obj.union.person_one.id,
-                    "target": chil.id,
+                    "target": child.id,
                     "type": "children",
-                    "strength": 0.5
+                    "strength": 0.2
                 })
         return data
