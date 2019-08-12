e872be56c29c650e456ab6d32c8c7233f9636871
JOAQUIMRGF <joaquim.rgf@gmail.com>
2019-08-12 10:15:43 +0100

    Add basic authentication and permissions

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,31 +1,46 @@
 from django.shortcuts import render, HttpResponse
+from django.views import View
+
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
 from .models import Person, Union, Family
 from .serializers import PersonSerializer, UnionSerializer, FamilySerializer, LinksSerializer
 
-from django.views import View
 
 # Create your views here.
-
 
 class PersonView(viewsets.ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class UnionView(viewsets.ModelViewSet):
     queryset = Union.objects.all()
     serializer_class = UnionSerializer
 
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
 
 class FamilyView(viewsets.ModelViewSet):
     queryset = Family.objects.all()
     serializer_class = FamilySerializer
 
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
 
 class LinksView(viewsets.ModelViewSet):
     queryset = Family.objects.all()
     serializer_class = LinksSerializer
+
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class Molecula(View):
